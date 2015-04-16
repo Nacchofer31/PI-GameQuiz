927e012fb4f9e926336f85b4e05ab60ae012d028
dai <dai@WINMAC14.alumnado.flo>
2015-04-16 20:19:54 +0200

    Ultima ACtu

--- ProyectoInicial/src/proyecto/vistas/PantallaHistoria.java
+++ ProyectoInicial/src/proyecto/vistas/PantallaHistoria.java
@@ -11,12 +11,20 @@ public class PantallaHistoria extends JPanel {
 	public PantallaHistoria() {
 		setLayout(null);
 		
+		Image iLetrasHistoria = new ImageIcon(this.getClass().getResource("/LetrasHistoria.png")).getImage();
+		JLabel labelLetrasHistoria = new JLabel("/LetrasHistoria.png");
+		labelLetrasHistoria.setBounds(-416, -25, 757, 205);
+		labelLetrasHistoria.setIcon(new ImageIcon(iLetrasHistoria));
+		add(labelLetrasHistoria);
+		
 		//IMAGEN DE FONDO
 		Image iFondo = new ImageIcon(this.getClass().getResource("/Fondo.png")).getImage();
 		JLabel imgFondo = new JLabel("/Fondo.png");
 		imgFondo.setBounds(0, 0, 800, 600);
 		imgFondo.setIcon(new ImageIcon(iFondo));
 		this.add(imgFondo);
+		
+		
 
 		
 	}
