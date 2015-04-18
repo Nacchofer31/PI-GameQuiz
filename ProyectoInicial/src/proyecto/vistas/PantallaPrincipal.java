be9dd33dd861a01e5ac08218c6cc0523e872528f
Nacchofer31 <nachopad31@hotmail.com>
2015-04-18 21:12:22 +0200

    Actualización del header de la app

--- ProyectoInicial/src/proyecto/vistas/PantallaPrincipal.java
+++ ProyectoInicial/src/proyecto/vistas/PantallaPrincipal.java
@@ -1,6 +1,7 @@
 package proyecto.vistas;
 
 import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,6 +11,8 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.Font;
+import java.awt.SystemColor;
 
 public class PantallaPrincipal extends JFrame {
 
@@ -63,76 +66,118 @@ public class PantallaPrincipal extends JFrame {
 		
 		// Boton Inicio
 		JButton btnInicio = new JButton("Inicio");
+		btnInicio.setIcon(new ImageIcon("C:\\Users\\Nach\\git\\PI-GameQuiz\\ProyectoInicial\\imgs\\home.png"));
+		btnInicio.setFont(new Font("Courgette", Font.PLAIN, 19));
+		btnInicio.setForeground(new Color(255, 0, 0));
+		btnInicio.setOpaque(false);
+		btnInicio.setContentAreaFilled(false);
+		btnInicio.setBorderPainted(false);
 		btnInicio.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				CardLayout c= (CardLayout)panelPantallas.getLayout();
 				c.show(panelPantallas, "PanelInicio");
 			}
 		});
-		btnInicio.setBounds(113, 25, 89, 23);
+		
+		Image imgHeader = new ImageIcon(this.getClass().getResource("/MuroPiedras.png")).getImage();
+		btnInicio.setBounds(94, 34, 128, 28);
 		panelBotones.add(btnInicio);
 		
 		//Boton Historia
 		JButton btnHistoria = new JButton("Historia");
+		btnHistoria.setIcon(new ImageIcon("C:\\Users\\Nach\\git\\PI-GameQuiz\\ProyectoInicial\\imgs\\book.png"));
+		btnHistoria.setFont(new Font("Courgette", Font.PLAIN, 19));
+		btnHistoria.setForeground(new Color(255, 0, 0));
+		btnHistoria.setOpaque(false);
+		btnHistoria.setContentAreaFilled(false);
+		btnHistoria.setBorderPainted(false);
 		btnHistoria.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				CardLayout c= (CardLayout)panelPantallas.getLayout();
 				c.show(panelPantallas, "PanelHistoria");
 			}
 		});
-		btnHistoria.setBounds(212, 25, 89, 23);
+		btnHistoria.setBounds(204, 30, 153, 36);
 		panelBotones.add(btnHistoria);
 		
 		//Boton Consolas
 		JButton btnConsolas = new JButton("Consolas");
+		btnConsolas.setIcon(new ImageIcon("C:\\Users\\Nach\\git\\PI-GameQuiz\\ProyectoInicial\\imgs\\consoles.png"));
+		btnConsolas.setFont(new Font("Courgette", Font.PLAIN, 19));
+		btnConsolas.setForeground(new Color(255, 0, 0));
+		btnConsolas.setOpaque(false);
+		btnConsolas.setContentAreaFilled(false);
+		btnConsolas.setBorderPainted(false);
 		btnConsolas.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				CardLayout c= (CardLayout)panelPantallas.getLayout();
 				c.show(panelPantallas, "PanelConsolas");
 			}
 		});
-		btnConsolas.setBounds(311, 25, 89, 23);
+		btnConsolas.setBounds(336, 30, 153, 36);
 		panelBotones.add(btnConsolas);
 		
 		//Boton Juegos
 		JButton btnJuegos = new JButton("Juegos");
+		btnJuegos.setIcon(new ImageIcon("C:\\Users\\Nach\\git\\PI-GameQuiz\\ProyectoInicial\\imgs\\games.png"));
+		btnJuegos.setFont(new Font("Courgette", Font.PLAIN, 19));
+		btnJuegos.setForeground(new Color(255, 0, 0));
+		btnJuegos.setOpaque(false);
+		btnJuegos.setContentAreaFilled(false);
+		btnJuegos.setBorderPainted(false);
 		btnJuegos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				CardLayout c= (CardLayout)panelPantallas.getLayout();
 				c.show(panelPantallas, "PanelJuegos");
 			}
 		});
-		btnJuegos.setBounds(410, 25, 89, 23);
+		btnJuegos.setBounds(452, 34, 146, 28);
 		panelBotones.add(btnJuegos);
 		
 		//Boton Quiz
 		JButton btnQuiz = new JButton("Quiz");
+		btnQuiz.setIcon(new ImageIcon("C:\\Users\\Nach\\git\\PI-GameQuiz\\ProyectoInicial\\imgs\\thumbs.png"));
+		btnQuiz.setFont(new Font("Courgette", Font.PLAIN, 19));
+		btnQuiz.setForeground(new Color(255, 0, 0));
+		btnQuiz.setOpaque(false);
+		btnQuiz.setContentAreaFilled(false);
+		btnQuiz.setBorderPainted(false);
 		btnQuiz.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				CardLayout c= (CardLayout)panelPantallas.getLayout();
 				c.show(panelPantallas, "PanelQuiz");
 			}
 		});
-		btnQuiz.setBounds(509, 25, 89, 23);
+		btnQuiz.setBounds(561, 28, 146, 41);
 		panelBotones.add(btnQuiz);
 		
 		//Boton Opciones
-		JButton btnOpciones = new JButton("Opciones");
+		JButton btnOpciones = new JButton("");
+		btnOpciones.setIcon(new ImageIcon("C:\\Users\\Nach\\git\\PI-GameQuiz\\ProyectoInicial\\imgs\\settings.png"));
+		btnOpciones.setFont(new Font("Courgette", Font.PLAIN, 19));
+		btnOpciones.setForeground(new Color(255, 0, 0));
+		btnOpciones.setOpaque(false);
+		btnOpciones.setContentAreaFilled(false);
+		btnOpciones.setBorderPainted(false);
 		btnOpciones.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				CardLayout c= (CardLayout)panelPantallas.getLayout();
 				c.show(panelPantallas, "PanelOpciones");
 			}
 		});
-		btnOpciones.setBounds(608, 25, 89, 23);
+		btnOpciones.setBounds(696, 28, 89, 53);
 		panelBotones.add(btnOpciones);
 		
 		//Logo GameQuiz
 		Image imgLogo = new ImageIcon(this.getClass().getResource("/LogoDef.png")).getImage();
 		JLabel logo = new JLabel("");
-		logo.setBounds(10, 0, 80, 84);
+		logo.setBounds(23, 1, 80, 84);
 		logo.setIcon(new ImageIcon(imgLogo));
 		panelBotones.add(logo);
+		JLabel header = new JLabel("");
+		header.setBounds(0, 0, 795, 85);
+		panelBotones.add(header);
+		header.setIcon(new ImageIcon(imgHeader));
 		
 		//MARCO BAJO BOTONES SOBRE PANTALLA
 		JPanel panel = new JPanel();
@@ -143,5 +188,4 @@ public class PantallaPrincipal extends JFrame {
 		
 		
 	}
-
 }
