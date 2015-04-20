b43cbb0bf4003fa9ab79b7dfe29a3c5c95b90520
dai <dai@WINMAC14.alumnado.flo>
2015-04-20 17:18:16 +0200

    Actualizacion "Historia"

--- ProyectoInicial/src/vista/PantallaHistoria.java
+++ ProyectoInicial/src/vista/PantallaHistoria.java
@@ -5,6 +5,10 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.Button;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Color;
 
 public class PantallaHistoria extends JPanel {
 	
@@ -17,12 +21,56 @@ public class PantallaHistoria extends JPanel {
 		labelLetrasHistoria.setIcon(new ImageIcon(iLetrasHistoria));
 		add(labelLetrasHistoria);
 		
+		//Boton Decada 70
+		JButton button70 = new JButton("70's");
+		button70.setBounds(45, 240, 58, 23);
+		add(button70);
+		
+		//Boton Decada 80
+		JButton button80 = new JButton("80's");
+		button80.setBounds(113, 240, 58, 23);
+		add(button80);
+		
+		//Boton Decada 90
+		JButton button90 = new JButton("90's");
+		button90.setBounds(181, 240, 58, 23);
+		add(button90);
+		
+		//Boton Decada 00's
+		JButton button00 = new JButton("00's");
+		button00.setBounds(249, 240, 58, 23);
+		add(button00);
+		
+		//Boton Decada 10's
+		JButton button10 = new JButton("10's");
+		button10.setBounds(317, 240, 58, 23);
+		add(button10);
+		
+		//Label elige
+		JLabel lblElige = new JLabel("Elige la década que quieras leer");
+		lblElige.setForeground(Color.WHITE);
+		lblElige.setFont(new Font("BatangChe", Font.BOLD, 20));
+		lblElige.setBounds(45, 174, 365, 33);
+		add(lblElige);
+		
 		//IMAGEN DE FONDO
 		Image iFondo = new ImageIcon(this.getClass().getResource("/Fondo.png")).getImage();
 		JLabel imgFondo = new JLabel("/Fondo.png");
 		imgFondo.setBounds(0, 0, 1600, 591);
 		imgFondo.setIcon(new ImageIcon(iFondo));
 		this.add(imgFondo);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 
