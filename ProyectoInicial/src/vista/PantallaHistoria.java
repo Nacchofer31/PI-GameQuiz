1fa570a252b65bbbe08d06d0c6eda9cb9b747a8e
Takuma <Takumakun@gmail.com>
2015-04-28 20:32:04 +0200

    Marcos de las decadas to puestos ahi en guapo lokos!!

--- ProyectoInicial/src/vista/PantallaHistoria.java
+++ ProyectoInicial/src/vista/PantallaHistoria.java
@@ -3,13 +3,13 @@ package vista;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class PantallaHistoria extends JPanel {
 	
@@ -18,13 +18,6 @@ public class PantallaHistoria extends JPanel {
 
 		setLayout(null);
 		setBounds(0, 0, 1355, 600);
-		
-		//Letras
-		Image iLetrasHistoria = new ImageIcon(this.getClass().getResource("/LetrasHistoria.png")).getImage();
-		JLabel labelLetrasHistoria = new JLabel("/LetrasHistoria.png");
-		labelLetrasHistoria.setBounds(63, 110, 357, 175);
-		labelLetrasHistoria.setIcon(new ImageIcon(iLetrasHistoria));
-		add(labelLetrasHistoria);
 		
 		
 		
@@ -35,7 +28,7 @@ public class PantallaHistoria extends JPanel {
 				PantallaPrincipal.ponerDecada70();
 			}
 		});
-		button70.setBounds(63, 432, 58, 23);
+		button70.setBounds(145, 140, 58, 23);
 		add(button70);
 		
 		//Boton Decada 80
@@ -45,7 +38,7 @@ public class PantallaHistoria extends JPanel {
 				PantallaPrincipal.ponerDecada80();
 			}
 		});
-		button80.setBounds(131, 432, 58, 23);
+		button80.setBounds(400, 140, 58, 23);
 		add(button80);
 		
 		//Boton Decada 90
@@ -55,7 +48,7 @@ public class PantallaHistoria extends JPanel {
 				PantallaPrincipal.ponerDecada90();
 			}
 		});
-		button90.setBounds(199, 432, 58, 23);
+		button90.setBounds(650, 140, 58, 23);
 		add(button90);
 		
 		//Boton Decada 00's
@@ -65,7 +58,7 @@ public class PantallaHistoria extends JPanel {
 				PantallaPrincipal.ponerDecada00();
 			}
 		});
-		button00.setBounds(267, 432, 58, 23);
+		button00.setBounds(900, 140, 58, 23);
 		add(button00);
 		
 		//Boton Decada 10's
@@ -75,14 +68,44 @@ public class PantallaHistoria extends JPanel {
 				PantallaPrincipal.ponerDecada10();
 			}
 		});
-		button10.setBounds(335, 432, 58, 23);
+		button10.setBounds(1150, 140, 58, 23);
 		add(button10);
+		
+		
+		Image marcoDecadas = new ImageIcon(this.getClass().getResource("/Marco.png")).getImage();
+		
+		JLabel lblMarco70 = new JLabel("/Marco.png");
+		lblMarco70.setBounds(75,200,200,350);
+		lblMarco70.setIcon(new ImageIcon(marcoDecadas));
+		this.add(lblMarco70);
+		
+		Image marco80 = new ImageIcon(this.getClass().getResource("/Marco80.png")).getImage();
+		JLabel lblMarco80 = new JLabel("/Marco80.png");
+		lblMarco80.setBounds(325,200,200,350);
+		lblMarco80.setIcon(new ImageIcon(marco80));
+		this.add(lblMarco80);
+		
+		JLabel lblMarco90 = new JLabel("/Marco.png");
+		lblMarco90.setBounds(575,200,200,350);
+		lblMarco90.setIcon(new ImageIcon(marcoDecadas));
+		this.add(lblMarco90);
+		
+		JLabel lblMarco00 = new JLabel("/Marco.png");
+		lblMarco00.setBounds(825,200,200,350);
+		lblMarco00.setIcon(new ImageIcon(marcoDecadas));
+		this.add(lblMarco00);
+
+		JLabel lblMarco10 = new JLabel("/Marco.png");
+		lblMarco10.setBounds(1075,200,200,350);
+		lblMarco10.setIcon(new ImageIcon(marcoDecadas));
+		this.add(lblMarco10);
+		
 		
 		//Label elige
 		JLabel lblElige = new JLabel("Elige la década que quieras leer");
 		lblElige.setForeground(Color.WHITE);
 		lblElige.setFont(new Font("BatangChe", Font.BOLD, 20));
-		lblElige.setBounds(63, 366, 365, 33);
+		lblElige.setBounds(475, 50, 365, 33);
 		add(lblElige);
 		
 		//IMAGEN DE FONDO
@@ -93,5 +116,4 @@ public class PantallaHistoria extends JPanel {
 		this.add(imgFondo);
 		
 	}
-
 }
